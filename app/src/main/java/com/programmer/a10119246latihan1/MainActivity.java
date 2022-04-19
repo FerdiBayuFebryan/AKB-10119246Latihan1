8dc7be7a308ff519102ea1c1762ebd17ff90194e
FerdiBayuFebryan <ferdi.10119246@mahasiswa.unikom.ac.id>
2022-04-19 16:37:58 +0700

    Latihan1

---
+++ app/src/main/java/com/programmer/a10119246latihan1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.programmer.a10119246latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//NIM                   : 10119246
+//NAMA                  : FERDI BAYU FEBRRYAN
+//TANGGAL PENGERJAAN    : 19 APRIL,2022
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
